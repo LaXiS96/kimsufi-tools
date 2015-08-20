cb952c799a224c739ae2385adb5cb71b37eee561
Antonio Ceccato <antonio.ceccato@hotmail.it>
2015-08-20 15:17:01 +0200

    Create lxc-delete.sh
    Stops and destroys named container

---
+++ lxc-delete.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+while [[ $# > 1 ]]; do
+  key="$1"
+  case $key in
+    -n|--name)
+      CONTAINER_NAME="$2"
+      shift
+    ;;
+    *)
+    ;;
+  esac
+  shift
+done
+
+if [[ -n $CONTAINER_NAME ]]; then
+  echo "Container name: $CONTAINER_NAME"
+else
+  echo "FATAL: missing container name. (specify with -n <name>)"
+  exit 1
+fi
+
+sudo lxc-stop -n $CONTAINER_NAME && sudo lxc-destroy -n $CONTAINER_NAME

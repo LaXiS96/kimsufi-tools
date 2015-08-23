278b1adf1c9e6c25131d3e8c01008bb921a2440b
Antonio Ceccato <antonio.ceccato@hotmail.it>
2015-08-23 14:49:55 +0200

    Update openvpn-setup.sh

--- openvpn-setup.sh
+++ openvpn-setup.sh
@@ -8,12 +8,13 @@ apt-get update
 apt-get install openvpn easy-rsa
 adduser --system --no-create-home --group openvpn
 
+rm -rf /etc/openvpn/easy-rsa
 mkdir /etc/openvpn/easy-rsa
 cd /etc/openvpn/easy-rsa
 cp /usr/share/easy-rsa/* .
 
 sed -i 's/export KEY_COUNTRY=.*/export KEY_COUNTRY="IT"/' vars
-sed -i 's/export KEY_PROVINCE=.*/export KEY_PROVINCE="TV"/' vars
+sed -i 's/export KEY_PROVINCE=.*/export KEY_PROVINCE="Treviso"/' vars
 sed -i 's/export KEY_CITY=.*/export KEY_CITY="Cavaso del Tomba"/' vars
 sed -i 's/export KEY_ORG=.*/export KEY_ORG="LaXiS"/' vars
 sed -i 's/export KEY_EMAIL=.*/export KEY_EMAIL="antonio.ceccato@hotmail.it"/' vars

0e9994dabe5d4ba1948c0c97dd081cdb95aa59fe
Antonio Ceccato <antonio.ceccato@hotmail.it>
2015-10-02 16:14:25 +0200

    Update trusty64-postinstall.sh

--- trusty64-postinstall.sh
+++ trusty64-postinstall.sh
@@ -88,15 +88,16 @@ cat > /etc/iptables.rules <<EOT
 COMMIT
 EOT
 #iptables-save > /etc/iptables.rules
-cat > /etc/network/if-pre-up.d/iptables <<EOT
-#!/bin/sh
-/sbin/iptables-restore -n < /etc/iptables.rules
-EOT
-chmod +x /etc/network/if-pre-up.d/iptables
+#cat > /etc/network/if-pre-up.d/iptables <<EOT
+##!/bin/sh
+#/sbin/iptables-restore -n < /etc/iptables.rules
+#EOT
+#chmod +x /etc/network/if-pre-up.d/iptables
 #echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
 #echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
 #apt-get -y install iptables-persistent
-
+echo
+echo "Please remember to add \"pre-up iptables-restore < /etc/iptables.rules\" to your main network interface!"
 echo "Done!"
 
 #reboot
